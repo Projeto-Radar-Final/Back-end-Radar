72248789e4308e7a95be8b77fa011ef6765b8536
Matheus <matheusgaldinoinfo@gmail.com>
2023-01-16 13:37:13 -0300

    Adicionando Projeto e entidade para modelagem

---
+++ Projeto-Radar/Entitys/Loja.cs
@@ -0,0 +1,6 @@
+﻿namespace Projeto_Radar.Entitys
+{
+    public class Loja
+    {
+    }
+}
---
+++ Projeto-Radar/Program.cs
@@ -0,0 +1,34 @@
+using System.Runtime.CompilerServices;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var conexao = Environment.GetEnvironmentVariable("DATABASE_URL_ESBOCO");
+Console.WriteLine(conexao);
+Console.WriteLine(conexao);
+Console.ReadKey();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
