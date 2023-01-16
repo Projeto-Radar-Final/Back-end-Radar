1deb0db276237e9fe54719933cb802ec310e434c
Matheus <matheusgaldinoinfo@gmail.com>
2023-01-16 14:48:57 -0300

    Subindo conexão com o banco antes da migration e a branch develop

--- Projeto-Radar/Entitys/PedidoProduto.cs
+++ Projeto-Radar/Entitys/PedidoProduto.cs
@@ -1,6 +1,31 @@
-﻿namespace Projeto_Radar.Entitys
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Projeto_Radar.Entitys
 {
-    public class PedidoProduto
+    [Table("tb_pedidoProduto")]
+    public record PedidoProduto
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("valor", TypeName = "DOUBLE")]
+        public double Valor { get; set; }
+
+        [Column("quantidade")]
+        public int quantidade { get; set; }
+
+        [ForeignKey("Produto")]
+        [Column("produto_id")]
+        public int? produtoId { get; set; }
+        public Produto Produto { get; set; }
+
+        [ForeignKey("Pedido")]
+        [Column("pedido_id")]
+        public int? PedidoId { get; set; }
+        public Pedido Pedido { get; set; }
+
     }
 }
