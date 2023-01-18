0bf08f868ede57aae4f09504a86d2485ef533ff5
Matheus <matheusgaldinoinfo@gmail.com>
2023-01-17 23:58:26 -0300

    Subindo algumas controllers e liberando cors

---
+++ Projeto-Radar/Controllers/PedidosController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Projeto_Radar.Context;
+using Projeto_Radar.Dtos;
+using Projeto_Radar.Entitys;
+using Projeto_Radar.Services;
+
+namespace Projeto_Radar.Controllers
+{
+    [Route("pedidos")]
+    [ApiController]
+    public class PedidosController : ControllerBase
+    {
+        private readonly DBContext _context;
+
+        public PedidosController(DBContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Pedido>>> GetPedidos()
+        {
+            if (_context.Pedidos == null)
+            {
+                return NotFound();
+            }
+            var pedidos = await _context.Pedidos.Include(c => c.Cliente).ToListAsync();
+
+            return StatusCode(200, pedidos);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Pedido>> GetPedido(int id)
+        {
+            if (_context.Pedidos == null)
+            {
+                return NotFound();
+            }
+            var pedido = await _context.Pedidos.FindAsync(id);
+
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            return StatusCode(200,pedido);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPedido([FromRoute] int id, Pedido pedido)
+        {
+            if (id != pedido.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(pedido).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PedidoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Pedido>> PostPedido(PedidoDto pedidodto)
+        {
+            var pedido = BuilderService<Pedido>.Builder(pedidodto);
+            if (_context.Pedidos == null)
+            {
+                return Problem("Entity set 'DBContext.Pedidos'  is null.");
+            }
+            _context.Pedidos.Add(pedido);
+            pedido.Data = DateOnly.FromDateTime(DateTime.Now);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPedido", new { id = pedido.Id }, pedido);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePedido(int id)
+        {
+            if (_context.Pedidos == null)
+            {
+                return NotFound();
+            }
+            var pedido = await _context.Pedidos.FindAsync(id);
+            if (pedido == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pedidos.Remove(pedido);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+
+        private bool PedidoExists(int id)
+        {
+            return (_context.Pedidos?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+ 
+}
